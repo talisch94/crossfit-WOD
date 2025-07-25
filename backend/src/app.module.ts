2ac03a87c34850e47ecc07e6eee9d3d890a58735
tali schvartz <talischvartz@gmail.com>
2025-07-25 17:34:05 +0300

    - Define DB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WodModule } from './wod/wod.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('DB_HOST'),
+                port: parseInt((configService.get('DB_PORT') ?? '10')),
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_NAME'),
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        }),
+        WodModule
+    ],
 })
-export class AppModule {}
+export class AppModule { }
