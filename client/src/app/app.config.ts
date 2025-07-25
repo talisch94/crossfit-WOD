a26205142282bd6d74085dfcabdaeacff179a802
tali schvartz <talischvartz@gmail.com>
2025-07-25 17:41:31 +0300

    - Fix provideHttpClient issue (Module is deprecated..)

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import routeConfig from './routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routeConfig), provideClientHydration(withEventReplay())]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routeConfig),
+        provideClientHydration(withEventReplay()),
+        provideHttpClient(withFetch(), withInterceptorsFromDi())
+    ]
 };
