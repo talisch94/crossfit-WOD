1cebf1a2219d50c8107a0857451c443b37452f4f
tali schvartz <talischvartz@gmail.com>
2025-07-26 16:46:37 +0300

    - Add tailwind styles to wod-form component -  Add create/edit logic to the component

--- client/src/app/components/wod-form/wod-form.component.ts
+++ client/src/app/components/wod-form/wod-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { WodService } from '../../services/wod/wod.service';
+import { Wod } from '../../interfaces/wod.interface';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     standalone: true,
@@ -10,8 +12,14 @@ import { WodService } from '../../services/wod/wod.service';
     templateUrl: './wod-form.component.html',
     styleUrl: './wod-form.component.scss'
 })
-export class WODFormComponent {
+export class WODFormComponent implements OnInit {
     private wodService = inject(WodService);
+    private route = inject(ActivatedRoute);
+    private router = inject(Router);
+
+    wodId = signal<string | null>(null);
+    isEditMode = computed(() => !!this.wodId());
+    wod = signal<Wod | null>(null);
 
     form = new FormGroup({
         name: new FormControl(''),
@@ -19,18 +27,53 @@ export class WODFormComponent {
         // isDone: new FormControl(false),
     });
 
-    saveWOD(): void {
-        this.wodService.submitWod(
-            this.form.value.name ?? '',
-            this.form.value.type ?? ''
-        ).subscribe({
-            next: (res) => {
-                console.log('WOD savedddd: ', res);
-                this.form.setValue({ name: '', type: '' });
+    ngOnInit(): void {
+        const id = this.route.snapshot.paramMap.get('id');
+        if (id) {
+            this.wodId.set(id);
+            this.loadWod(id);
+        }
+    }
+
+    private loadWod(id: string) {
+        this.wodService.getWodById(id).subscribe({
+            next: (data) => {
+                this.wod.set(data as Wod);
+                this.form.patchValue(data);
             },
             error: (err) => {
-                console.error('Error saving wod: ', err)
-            }
-        })
+                console.error('Failed to fetch WOD', err);
+            },
+        });
+    }
+
+    saveWOD(): void {
+        if (this.form.invalid) return;
+
+        const wodData = this.form.value;
+        const wod = ({ name: wodData.name ?? 'Name', type: wodData.type ?? 'Type' }) as Wod;
+
+        if (this.isEditMode()) {
+            this.wodService.updateWod(this.wodId()!, wod).subscribe({
+                next: (res) => {
+                    console.log('WOD updated successfully: ', res);
+                    this.form.setValue({ name: '', type: '' });
+                    this.router.navigate(['/wod-list']);
+                },
+                error: (err) => {
+                    console.error('Error saving wod: ', err)
+                }
+            })
+        } else {
+            this.wodService.createWod(wod).subscribe({
+                next: (res) => {
+                    console.log('WOD created successfully: ', res);
+                    this.router.navigate(['/wod-list']);
+                },
+                error: (err) => {
+                    console.error('Error saving wod: ', err)
+                }
+            });
+        }
     }
 }
