79e4504e9a94dc5bed3a17ee654246a91ebb76d9
tali schvartz <talischvartz@gmail.com>
2025-07-27 11:43:39 +0300

    - Create data-source file for TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,27 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WodModule } from './wod/wod.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { dataSourceOptions } from './data-source';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get('DB_HOST'),
-                port: parseInt((configService.get('DB_PORT') ?? '10')),
-                username: configService.get('DB_USERNAME'),
-                password: configService.get('DB_PASSWORD'),
-                database: configService.get('DB_NAME'),
-                autoLoadEntities: true,
-                synchronize: true,
-            }),
-            inject: [ConfigService],
-        }),
+        TypeOrmModule.forRoot(dataSourceOptions),
         WodModule
     ],
 })
