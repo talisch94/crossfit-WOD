1b0bef325a23a97498dace1dc7a3909e96382d07
tali schvartz <talischvartz@gmail.com>
2025-07-27 11:44:19 +0300

    - Rename 'wod' table to 'wods - Create migration

--- backend/src/wod/wod.entity.ts
+++ backend/src/wod/wod.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-@Entity()
+@Entity('wods')
 export class Wod {
     @PrimaryGeneratedColumn('uuid')
     id: string;
