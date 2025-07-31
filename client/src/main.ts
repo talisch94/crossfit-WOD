e30e3f1e12f9d1cc5bebec498528aa1269860f36
talisch94 <talischvartz@gmail.com>
2025-07-31 10:44:13 +0300

    - Add ngrx packages

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,12 +3,19 @@ import { appConfig } from './app/app.config';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
 import { AppComponent } from './app/app.component';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
     ...appConfig,
     providers: [
-        ...(appConfig.providers || []),
-        provideRouter(routeConfig),
-        provideProtractorTestingSupport()
-    ]
+    ...(appConfig.providers || []),
+    provideRouter(routeConfig),
+    provideProtractorTestingSupport(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 });
