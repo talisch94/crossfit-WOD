d28baf029e210f6ffae95a4c6129feef2d11c8c5
talisch94 <talischvartz@gmail.com>
2025-08-03 00:43:20 +0300

    - Update package.json - Move appConfig to a separate file

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { authReducer } from './store/auth.reducer';
 
 import routeConfig from './routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -10,6 +15,9 @@ export const appConfig: ApplicationConfig = {
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routeConfig),
         provideClientHydration(withEventReplay()),
-        provideHttpClient(withFetch(), withInterceptorsFromDi())
+        provideHttpClient(withFetch(), withInterceptorsFromDi()),
+        provideStore({ auth: authReducer }),
+        provideEffects(),
+        provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     ]
 };
